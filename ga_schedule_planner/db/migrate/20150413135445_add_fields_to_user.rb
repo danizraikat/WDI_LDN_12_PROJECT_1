c4839250e5667adbdf6171e573a1b44bd9426b90
danizraikat <danizraikat@gmail.com>
2015-04-13 14:58:57 +0100

    added fields to the User model

---
+++ ga_schedule_planner/db/migrate/20150413135445_add_fields_to_user.rb
@@ -0,0 +1,10 @@
+class AddFieldsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :role, :string
+    add_column :users, :first_name, :string
+    add_column :users, :second_name, :string
+    add_column :users, :password_confirmation, :string
+    add_column :users, :gender, :string
+    add_column :users, :tel_number, :string
+  end
+end
