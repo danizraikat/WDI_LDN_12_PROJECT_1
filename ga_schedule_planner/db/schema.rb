c88e4dad59f10cf966a2ad782ae24b86a58ffc4b
danizraikat <danizraikat@gmail.com>
2015-04-14 11:20:33 +0100

    course_type scaffold generaterd

--- ga_schedule_planner/db/schema.rb
+++ ga_schedule_planner/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413211909) do
+ActiveRecord::Schema.define(version: 20150414101939) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer  "user_id"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20150413211909) do
     t.boolean  "paid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "course_types", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "courses", force: :cascade do |t|
