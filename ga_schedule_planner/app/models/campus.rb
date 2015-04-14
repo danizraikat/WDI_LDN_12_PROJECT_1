119c0f5b865cc20899c144940a72db2dfee11ee6
danizraikat <danizraikat@gmail.com>
2015-04-14 12:30:55 +0100

    created all associations I think

--- ga_schedule_planner/app/models/campus.rb
+++ ga_schedule_planner/app/models/campus.rb
@@ -1,2 +1,4 @@
 class Campus < ActiveRecord::Base
+  has_many :classrooms
+  has_many :courses, through: :classrooms 
 end
--- ga_schedule_planner/app/models/course.rb
+++ ga_schedule_planner/app/models/course.rb
@@ -3,4 +3,5 @@ class Course < ActiveRecord::Base
   has_and_belongs_to_many :users, through: :bookings 
   belongs_to :course_type
   belongs_to :classroom 
+  belongs_to :campus, through: :classrooms 
 end
