6fd0b9a025fde31e548b9128be63d1823935bbf5
danizraikat <danizraikat@gmail.com>
2015-04-15 13:46:32 +0100

    navigation top bar added

--- ga_schedule_planner/app/models/user.rb
+++ ga_schedule_planner/app/models/user.rb
@@ -7,15 +7,15 @@ class User < ActiveRecord::Base
   has_many :courses, through: :bookings
   validates :role, :first_name, :second_name, :gender, :tel_number, presence: true 
   validates :tel_number, numericality: true 
-  validates :gender, inclusion: { in: %w(male female),
+  validates :gender, inclusion: { in: %w(Male Female),
     message: "%{value} is not a gender" }
-  validates :role, inclusion: { in: %w(student instructor admin),
+  validates :role, inclusion: { in: %w(Student Instructor Admin),
     message: "%{value} is not a GA role" }
   validates :first_name, :second_name, format: { with: /\A[a-zA-Z]+\z/,
     message: "only allows letters" }
 
 end
-
+ 
   # gender as male or female
   
   # role out of an array of roles
