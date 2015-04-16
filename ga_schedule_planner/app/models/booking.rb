c9d6d12cc7eb2e60df18d6fe5ae8643ced0aae81
danizraikat <danizraikat@gmail.com>
2015-04-16 16:50:05 +0100

    uploader for campus installed

--- ga_schedule_planner/app/models/booking.rb
+++ ga_schedule_planner/app/models/booking.rb
@@ -2,6 +2,5 @@ class Booking < ActiveRecord::Base
   belongs_to :user
   belongs_to :course 
   validates :user_id, :course_id, presence: true
-  
   validates :paid, :inclusion => {:in => [true]}
 end
